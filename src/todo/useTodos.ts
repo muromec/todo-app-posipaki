40cbce6316d355b0a04e6b085244c94c464353ca
Illya Petrov <ilya.muromec@gmail.com>
2023-06-13 19:57:08 +0200

    more typescript

--- src/todo/useTodos.ts
+++ src/todo/useTodos.ts
@@ -3,7 +3,7 @@ import { useResource } from 'react-pspki';
 import { TodoItems } from './types';
 const NO_ITEMS: TodoItems = [];
 
-export function useTodos() {
+export function useTodos() : { isLoading: boolean, items: TodoItems } {
   const url = new URL('/api/todo/', document.location.origin);
   const { items = NO_ITEMS, isLoading } = useResource<TodoItems>({ url, id: 'todos' });
   return { items, isLoading};
